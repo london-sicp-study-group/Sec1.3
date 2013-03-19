a1e6b644d69419d5a36ddf4e663e71f8a49d5541
teo <teo.danciu@gmail.com>
2013-03-19 13:21:11 +0000

    Minor formatting

--- reverse.rkt
+++ reverse.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
-;Exercise 2.18 Define a procedure reverse that takes a list as argument and returns a list of the same 
-;elements in reverse order:
-;(reverse (list 1 4 9 16 25))
-;(25 16 9 4 1)
+; Exercise 2.18 Define a procedure reverse that takes a list as argument and returns a list of the same 
+; elements in reverse order:
+; (reverse (list 1 4 9 16 25))
+; (25 16 9 4 1)
 
 (require "../common.rkt")
 
@@ -19,14 +19,14 @@
       l
       (append (reverse-with-append (cdr l)) (list (car l)))))
 
-;Exercise 2.27.  Modify your reverse procedure of exercise 2.18 to produce a deep-reverse 
-;procedure that takes a list as argument and returns as its value the list with its elements reversed and with all 
-;sublists deep-reversed as well. For example,
-;(define x (list (list 1 2) (list 3 4)))
-;(reverse x)
-;((3 4) (1 2))
-;(deep-reverse x)
-;((4 3) (2 1))
+; Exercise 2.27.  Modify your reverse procedure of exercise 2.18 to produce a deep-reverse 
+; procedure that takes a list as argument and returns as its value the list with its elements reversed and with all 
+;s ublists deep-reversed as well. For example,
+; (define x (list (list 1 2) (list 3 4)))
+; (reverse x)
+; ((3 4) (1 2))
+; (deep-reverse x)
+; ((4 3) (2 1))
 
 (define (deep-reverse l)
   (define (deep-reverse0 l acc)
